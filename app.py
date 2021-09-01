0944e9ed7fb8d68f4142aa6e9bd94b818a3ba3b0
Michał K <50676536+Arivalo@users.noreply.github.com>
2021-09-01 15:21:04 +0200

    formatowanie

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 from PIL import Image
 from funkcje import *
+import matplotlib.dates as mdates
 
 
 @st.cache(suppress_st_warning=True)
@@ -86,7 +87,7 @@ def tabela_statystyk_dnia(df):
     dane_dnia['Energia hydrauliczna w ciągu dnia [kJ]'] = [df['hydraulic_energy'].max()]
     dane_dnia['Energia hydrauliczna zagęszczania [kJ]'] = [df['energia_hydr_zageszczania'].max()]
 
-    dane_dnia = dane_dnia.T.rename(columns={0:""})
+    dane_dnia = dane_dnia.T.rename(columns={0:"Wartość"})
     #dane_dnia.columns = dane_dnia.iloc[0]
     #dane_dnia = dane_dnia.iloc[1:]
     
@@ -102,19 +103,21 @@ im_sketch = "res/sketch_lowres.png"
 im_central = "res/drawing.PNG"
 im_logo = "res/logo_xt.png"
 im_logo2 = "res/logo_zoeller.png"
+im_table = "res/tabelka.png"
 
 sketch = Image.open(im_sketch).convert('RGB')
 central_sketch = Image.open(im_central).convert('RGB')
 xt_logo = Image.open(im_logo).convert('RGB')
 zoe_logo = Image.open(im_logo2).convert('RGB')
+tabela_info = Image.open(im_table).convert('RGB')
 
-## HEADER
+### HEADER ###
 
 ## c1
 c1.title("WIP")
 c1.image(sketch, use_column_width=True)
  
-c1.header("Day selection:")
+c1.header("Wybór dnia:")
 
 data_od = c1.date_input("", value=dt.date(2021,8,3), min_value=dt.date(2021,8,1), max_value=dt.date.today())
 c1.write("------------------")
@@ -128,12 +131,13 @@ c2.image(central_sketch, use_column_width=True)
 
 
 ## c3
-c3.title("WIP")
+#c3.title("WIP")
 c3.image(xt_logo, use_column_width=True)
 c3.image(zoe_logo, use_column_width=True)
+c3.image(tabela_info, use_column_width=True)
 
 
-## MAIN
+### MAIN ###
 
 url = utworz_url(data_od, data_od)
 
@@ -142,41 +146,49 @@ try:
     df = download_data(url)
 
     dane_z_dnia = tabela_statystyk_dnia(df)
+    
     c2.write(f"Statystyki z {data_od}:")
     #c2.dataframe(dane_z_dnia, height=500)
     
+   
     c3.table(dane_z_dnia)
-
-    c2.markdown(get_table_download_link(dane_z_dnia, f'diagnostics_{data_od}'), unsafe_allow_html=True)
+    c1.markdown(get_table_download_link(dane_z_dnia, f'diagnostics_{data_od}'), unsafe_allow_html=True)
     
-    ## WYKRESY
+    
+    ## WYKRESY ##
 
     
-    cols = st.columns((1,2,1))
+    cols = st.columns((1,1,1))
 
     fig = px.line(df, x='Data_godzina', y='Nacisk_total', title="Nacisk na osie w trakcie dnia")
-
-
-    fig_p = plt.figure(figsize=(14,4))
+    
+    
+    ## WYKRESY DOLNE ##
+    xfmt = mdates.DateFormatter('%H:%M')
+    
+    fig_p0, ax_0 = plt.subplots(1, figsize=(8,5))
     plt.plot(df['Data_godzina'], df['Fuel_consumption'])
     plt.title("Zużycie paliwa", fontsize=24)
+    ax_0.xaxis.set_major_formatter(xfmt)
     plt.tight_layout()
 
-    fig_p1 = plt.figure(figsize=(14,4))
+    fig_p1, ax_1 = plt.subplots(1, figsize=(8,5))
     plt.plot(df['Data_godzina'], df['predkosc_osi'])
     plt.title("Prędkość w trakcie dnia", fontsize=24)
+    ax_1.xaxis.set_major_formatter(xfmt)
     plt.tight_layout()
 
-    fig_p2 = plt.figure(figsize=(14,4))
+    fig_p2, ax_2 = plt.subplots(1, figsize=(8,5))
     plt.plot(df['Data_godzina'], df['motogodziny_total'])
     plt.title("Motogodziny", fontsize=24)
+    ax_2.xaxis.set_major_formatter(xfmt)
     plt.tight_layout()
 
     c2.plotly_chart(fig)
 
-    cols[1].write(fig_p)
+    cols[0].write(fig_p0)
     cols[1].write(fig_p1)
-    cols[1].write(fig_p2)
+    cols[2].write(fig_p2)
     #cols[2].plotly_chart(fig)
     
     
