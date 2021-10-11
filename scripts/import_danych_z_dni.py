5f8f521368a67b2ab803c107b6d5731fcf529b5a
Michał K <50676536+Arivalo@users.noreply.github.com>
2021-10-11 17:04:17 +0200

    aktualna funkcja do pobierania danych

--- scripts/import_danych_z_dni.py
+++ scripts/import_danych_z_dni.py
@@ -38,7 +38,8 @@ def utworz_url(nr_mon, od_k, do_k, godz_od, godz_do):
                  f"to={do_kiedy}T{do_kiedy_godzina}:00Z&monitoredId={nr_monitorowany}&limit=5000000"
 
     return url_string
-def pobierz_dane(url, haslo='', login='wysocki;2139'):
+    
+def pobierz_dane(url, haslo='', login='wysocki;2139', retry=5):
     '''
     Pobiera dane z serwera Xtrack wedlug danego linku "url" i wyznacza kolumny z danymi GPS
     
@@ -47,6 +48,7 @@ def pobierz_dane(url, haslo='', login='wysocki;2139'):
     url <String> - pelny link z zapytaniem
     haslo <String> - haslo do autentykacji BASIC
     login <String> - (default: "wysocki;2139") login do autentykacji BASIC
+    retry <number< - (default: 5) liczba powtorzen prob pobierania w przypadku bledu
     
     Wyjscie:
     
@@ -54,26 +56,44 @@ def pobierz_dane(url, haslo='', login='wysocki;2139'):
     
     '''
 
-    r = requests.get(url,auth=(login, haslo))
+    i = 0
 
-    j = r.json()
+    while i < retry:
+        r = requests.get(url,auth=(login, haslo))
+
+        try:
+            j = r.json()
+            break
+        except:
+            i += 1
+            print(f"Try no.{i} failed")
+
+    if i == retry:
+        print(f"Failed to fetch data for: {url}")
+        return pd.DataFrame()
+        
+    df = pd.DataFrame.from_dict(j['entities'])
     
-    try:
-        df = pd.DataFrame.from_dict(j['entities'])
-    except KeyError:
-        print(j)
-        print(".\n.\n.\nBlad wczytywania")
-
     if not df.empty:
         try:
             df['longtitude'] = [x['coordinates']['x'] for x in df['_meta']]
             df['latitude'] = [y['coordinates']['y'] for y in df['_meta']]
             df.pop('_meta')   
+            
         except KeyError:
-            print(f'Problem z url: {url}')
-
-    
+            print(f'Url error: {url}')
+        
+        df.ffill(inplace=True)
+        df = df.fillna(0)
+        try:
+            df['updatedAt'] = pd.to_datetime(df['updatedAt']).dt.tz_localize(None)
+        except ValueError:
+            pass
+        except KeyError:
+            pass
+            
     return df
+    
 def pobierz_i_wstepnie_przygotuj_dane(haslo, nr_mon, od_k, do_k, godz_od, godz_do):
     '''
     Pobiera i przygotowuje dane - wypelnia NaN poprzednimi wartosciami, reszte zeruje
